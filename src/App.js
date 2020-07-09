2718f4e57df67c8924f8cf00f6784742b307248a
Khamari Thompson <61212885+Khamari13@users.noreply.github.com>
2020-07-09 10:42:56 -0400

    updated dataBody.jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Data from './components/Data'
+import Data from './components/Data.jsx'
 
 function App() {
   return (
