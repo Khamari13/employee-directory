b1c8d024f04614bc3bd549a0eb3145709da32d08
Khamari Thompson <61212885+Khamari13@users.noreply.github.com>
2020-07-09 01:43:07 -0400

    updated dataBody.jsx

--- src/components/DataBody.jsx
+++ src/components/DataBody.jsx
@@ -4,10 +4,10 @@ const DataBody = props => {
     console.log(props.user)
     return (
         <card>
-            <div  className="col"><img src= {props.user.picture.thumbnail}/></div>
-            <div  className="col">{`${props.user.name.first} ${props.user.name.last}`}</div>
-            <div  className="col">{props.user.email}</div>
-            <div  className="col">{props.user.cell}</div>
+            <div className="col"><img src= {props.user.picture.thumbnail}/></div>
+            <div className="col">{`${props.user.name.first} ${props.user.name.last}`}</div>
+            <div className="col">{props.user.email}</div>
+            <div className="col">{props.user.cell}</div>
         </card>
     )
 }
